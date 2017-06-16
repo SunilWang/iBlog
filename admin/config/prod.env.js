1ac9e06fcf177e9194f4f3da0ab2a9471de128a8
SunilWang <ahwangshu@qq.com>
2017-06-16 13:39:00 +0800

    update api url

--- admin/config/prod.env.js
+++ admin/config/prod.env.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var merge = require('webpack-merge')
 var config = {
   NODE_ENV: '"production"',
-  api: '"http://localhost:3300/api/"'
+  api: '"http://api.sunil.wang/api/"'
 }
 
 if (process.env.NODE_ENV === 'production') {

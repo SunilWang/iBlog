817680c5f71899358cd38f0d1ea5f64891204e25
SunilWang <ahwangshu@qq.com>
2017-06-17 22:01:59 +0800

    删除提示消息组件

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,6 @@
 
 import './stylus/index.styl'
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import MessageBox from 'vue-msgbox'
 import adminComponent from './Admin.vue'
 import {router} from './router.js'
 
@@ -17,10 +15,6 @@ require('font-awesome/css/font-awesome.min.css')
  * 导致store在login组件拿到的是个空对象,所以就让router.js提前执行完毕,把router.beforeEach放这了.
  * */
 
-window.alert = MessageBox
-
 Vue.filter('md2Text', md2Text)
-
-window.alert = MessageBox
 
 router.start(adminComponent, '#app')

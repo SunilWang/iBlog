b4f0bd207d368f9c9c427ddcf2324f62421fc222
SunilWang <ahwangshu@qq.com>
2017-09-10 16:08:02 +0800

    修改 logo

--- server/configs/dev.js
+++ server/configs/dev.js
@@ -11,18 +11,11 @@ module.exports = {
   'jwt': {
     'cert': 'sunil-dev'
   },
-  // qiniu: {
-  //   IMAGE_STYLE: '-sy_sunil_wang_01',
-  //   ACCESS_KEY: '',
-  //   SECRET_KEY: '',
-  //   BUCKET: '',
-  //   DOMAIN: ''
-  // },
   qiniu: {
     IMAGE_STYLE: '-sy_sunil_wang_01',
-    ACCESS_KEY: '1TyBsVj4kyIkVZjhjCzf0Ukh5mrddGeMYvyUIGld',
-    SECRET_KEY: 'p1SukvLFMElAwR6TSNo9Tsf57UqxOi7gWjIlDL-F',
-    BUCKET: 'iblog',
-    DOMAIN: 'https://qnstatic.sunil.wang'
+    ACCESS_KEY: '',
+    SECRET_KEY: '',
+    BUCKET: '',
+    DOMAIN: ''
   }
 }

4daab04c409d8c978e3381c59a870dd35a5cc07f
SunilWang <ahwangshu@qq.com>
2017-09-10 17:15:31 +0800

    添加 畅言测试

--- client/src/components/common/ChangYan.vue
+++ client/src/components/common/ChangYan.vue
@@ -9,12 +9,12 @@
       id: {
         type:String
       }
-    },
-    ready(){
-      window.changyan.api.config({
-        appid: 'cytcWetJR',
-        conf: 'prod_da07b8cf59b89bbe35d1d764199965b2'
-      });
-    }
+//    },
+//    ready(){
+//      window.changyan.api.config({
+//        appid: 'cytcWetJR',
+//        conf: 'prod_da07b8cf59b89bbe35d1d764199965b2'
+//      });
+//    }
   }
 </script>

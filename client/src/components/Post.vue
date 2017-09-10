42cafce3de235bc3536c21dcf6f4c7da9ed0e761
SunilWang <ahwangshu@qq.com>
2017-09-10 16:38:28 +0800

    添加 畅言测试

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -17,6 +17,16 @@
     </article>
     <pagination :next="nextArticle !== null" :next-link="nextArticle?'/posts/'+nextArticle._id:''" :next-word="nextArticle&&nextArticle.title" :prev="prevArticle !== null" :prev-link="prevArticle?'/posts/'+prevArticle._id:''" :prev-word="prevArticle&&prevArticle.title" ></pagination>
   </div>
+  <!--高速版-->
+  <div id="SOHUCS" sid="{{id}}"></div>
+  <script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
+  <script type="text/javascript">
+    window.changyan.api.config({
+      appid: 'cytcWetJR',
+      conf: 'prod_da07b8cf59b89bbe35d1d764199965b2'
+    });
+  </script>
+
 </template>
 <style lang="stylus">
   @import "../stylus/_settings.styl"

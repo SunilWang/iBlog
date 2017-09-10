65385c59bd76a5f27b22cad2abbb4138f00bd21d
SunilWang <ahwangshu@qq.com>
2017-09-10 17:18:47 +0800

    添加 畅言测试

--- client/src/components/common/ChangYan.vue
+++ client/src/components/common/ChangYan.vue
@@ -2,12 +2,6 @@
   <!--高速版-->
   <div id="SOHUCS" sid="{{id}}"></div>
   <script charset="utf-8" type="text/javascript" src="//changyan.sohu.com/upload/changyan.js" ></script>
-  <script type="text/javascript">
-    window.changyan.api.config({
-      appid: 'cytcWetJR',
-      conf: 'prod_da07b8cf59b89bbe35d1d764199965b2'
-    });
-  </script>
 </template>
 <script>
   export default {
@@ -15,6 +9,12 @@
       id: {
         type:String
       }
+    },
+    ready(){
+      window.changyan.api.config({
+        appid: 'cytcWetJR',
+        conf: 'prod_da07b8cf59b89bbe35d1d764199965b2'
+      });
     }
   }
 </script>
